814c3f96ece2f4f9e975f556dab24f5ea4ab0725
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-26 22:04:53 +0600

    accordion added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import InterviewQuestions from './components/InterviewQuestions/InterviewQuestions';
 import Shop from './components/Shop/Shop';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <h1 className='text-center text-info'>Welcome to Dream Laptop Store</h1>
       <Shop />
+      <InterviewQuestions/>
       <Footer/>
     </div>
   );
