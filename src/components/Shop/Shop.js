7fb7860c9be10e14ae18d42fc26d1b2f8a4a3060
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-26 11:09:47 +0600

    react hooks added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css';
 const Shop = () => {
+   
+   const [laptops, setLaptops] = useState([]);
+       
+   useEffect(() => {
+      fetch('data.json')
+         .then(res => res.json())
+         .then(data => setLaptops(data));
+   }, [laptops])
+   
    return (
       <div className='row'>
          <div className="col col-md-3">
-            <h1>This is products </h1>
+            <h1>This is products:{laptops.length} </h1>
          </div>
          <div className="col">
             <h1>This is cart</h1>
