e746c3c273b246a9233e38279661f95300b85740
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-26 07:39:47 +0600

    react bootstrap and heroicons added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi there</h1>
+    <div>
+      <Header></Header>
     </div>
   );
 }
