f028f06d844e26654a10a8d914f9daa5b8eebc57
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-26 10:55:40 +0600

    style updated and data.json updated

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Shop.css';
-
 const Shop = () => {
    return (
-      <div>
-         
+      <div className='row'>
+         <div className="col col-md-3">
+            <h1>This is products </h1>
+         </div>
+         <div className="col">
+            <h1>This is cart</h1>
+         </div>
       </div>
    );
 };
