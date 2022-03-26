27022769651c76581f5de7370a1ea470f61340ce
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-26 08:45:49 +0600

    header component complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,24 @@
-import React from 'react';
-
+import React from "react";
+import "./Header.css";
 const Header = () => {
-   return (
-      <div>
-         <h1>Header</h1>
+  return (
+    <nav className="bg-light text-black nav justify-content-between align-items-center navigation">
+      <div className="ps-5">
+        <h2>Dream Store</h2>
       </div>
-   );
+      <div>
+        <a className="mx-3" href="/">
+          Home
+        </a>
+        <a className="mx-3" href="/about">
+          About us
+        </a>
+        <a className="mx-3" href="/cart">
+          Cart
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
