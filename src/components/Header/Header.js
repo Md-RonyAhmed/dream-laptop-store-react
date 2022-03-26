706f0a543e4a0d368547b0470a2fc32821679996
Md. Rony <rony15-10527@diu.edu.bd>
2022-03-26 14:56:36 +0600

    fontawesome cart\ added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,22 +2,22 @@ import React from "react";
 import "./Header.css";
 const Header = () => {
   return (
-    <nav className="text-black nav justify-content-between align-items-center navigation">
-      <div className="ps-5">
-        <h2>Dream Store</h2>
-      </div>
-      <div>
-        <a className="mx-3" href="/">
-          Home
-        </a>
-        <a className="mx-3" href="/about">
-          About us
-        </a>
-        <a className="mx-3" href="/cart">
-          Cart
-        </a>
-      </div>
-    </nav>
+      <nav className="text-black nav justify-content-between align-items-center navigation">
+        <div className="ps-5">
+          <h2>Dream Store</h2>
+        </div>
+        <div>
+          <a className="mx-3" href="/">
+            Home
+          </a>
+          <a className="mx-3" href="/about">
+            About us
+          </a>
+          <a className="mx-3" href="/cart">
+            Cart
+          </a>
+        </div>
+      </nav>
   );
 };
 
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 const Product = (props) => {
   const { name, brand, price, pic } = props.product;
   return (
     <div className="product">
       <img src={pic} alt=""></img>
-        <h3>Brand: {brand}</h3>
-        <h4>Model: {name}</h4>
-        <h4>Price: $ {price}</h4>
+      <h4>Brand: {brand}</h4>
+      <h6>Model: {name}</h6>
+      <h5>Price: $ {price}</h5>
+      <button type="button" className="btn btn-primary d-block w-100">
+        Add to cart <FontAwesomeIcon icon={faShoppingCart} />
+      </button>
     </div>
   );
 };
